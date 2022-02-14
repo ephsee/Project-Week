b805bde921d6106a4666c525a8424d834f7c09bf
Francois Spies <fcspies@gmail.com>
2022-02-14 12:59:38 -0500

    clean up

--- humans-helping-humans/src/App.js
+++ humans-helping-humans/src/App.js
@@ -1,34 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Home, {useEffect, UseState} from './Home';
+import Home from './Home';
 
 function App() {
-
-  
-
   return (
-    <div className="humans-helping-humans">
-      <Home />
-    </div>
-    
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Home />
+          </header>
+      </div>
+    </>
   );
 }
 
 export default App;
-
-// Ifdiv className="App">
-{/* <header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
-</header>
-</div> */}
