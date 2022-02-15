587c4ab49aa43891fde9c4a4aea88cdea02bc31f
Lara <laraellenh@gmail.com>
2022-02-15 18:08:06 -0500

    about changes

--- humans-helping-humans/src/About.js
+++ humans-helping-humans/src/About.js
@@ -6,7 +6,15 @@ function About() {
             <h3> About Humans Helping Humans </h3>
             {/* <img src="./assets/PNG image.png" alt='lara'/> */}
             <img src={require('./assets/PNG image.png')} />
-            <p> something nice about the history and purpose of the app </p>
+            <p> This app is a combination of inspirations. First, the endless impact teachers, mentors, and friends have on our lives and the collectie good we enjoy from those lessons learned. Second, the amazing resource of scientific data, distilled by neorbiologist and optimologist Dr. Andrew Huberman. His free of cost podcast/youtube channel distills meaning and actionable ideas for optimizing health. On this site, visit the resource tab to learn about the scientific beenfits of gratitiude, and to dispell the myths of gratitude. On the Stories tab, hear how others have been impacted by their teachers and mentors. TRHe benefit of hearing and sharing the narrative of  humans helping humans is undeniedable!
+
+                While here:
+                 -Click on the resources tab to watch Dr. Huberman lecture on the science of gratitude.
+                 -Click on the stories tab to read stories about inspiring teachers, and boost your serontonin as a result.
+                 -Click to learn a random fact about gratitude on the Resource tab.
+                 -Click on the stories tab to share your own inspirational story of being helped, and boost someone's serotonin.
+                 -And remember, when you need a brain boost, think of these stories to imrpove your mood and sense of contentment.
+            </p>
         </div>
     )
 }
